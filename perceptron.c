926e0206551c8ccf2675e13ba2c0b634143e66fb
deepg14 <deepankarg14@gmail.com>
2018-02-01 03:16:38 -0500

    perceptron skeleton set up, training method framework added

---
+++ perceptron.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//Macros
+#define THRESHOLD 0.5
+#define TRAINING_DATA "training_data.txt"
+#define TEST_DATA "test_data.txt"
+#define LEARNING_RATE 0.2
+
+int main(void) {
+    //TRAINING
+
+    //TEST
+}
